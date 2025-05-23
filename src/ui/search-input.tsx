4874730edb7dc65c3d363b6c3ed68e7f67cb8bd1
Niks Birzgalis <hello@niksbirzgalis.com>
2025-05-23 16:52:12 +0000

    chore: minor search input component cleanup

--- src/ui/search-input.tsx
+++ src/ui/search-input.tsx
@@ -22,13 +22,13 @@ function SearchInput({
   };
 
   return (
-    <div className="relative bg-white">
+    <div className="relative">
       <input
         value={search.value}
         onFocus={handleFocus}
         onChange={handleChange}
         placeholder={placeholder}
-        className="w-full rounded border border-gray-300 p-2 pr-10"
+        className={`w-full rounded border border-gray-300 bg-white p-2 ${search.query.isLoading ? 'pr-10' : ''}`}
       />
       {search.query.isLoading && (
         <div className="absolute top-1/2 right-1 size-4 -translate-x-1/2 -translate-y-1/2 animate-spin rounded-full border-2 border-t-transparent" />
